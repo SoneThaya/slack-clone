6bc8a7c8122cf4abf2b7aff438be28ecb54934dc
Sone <sthaya@gmail.com>
2020-08-30 14:24:28 -0700

    added inputs for register component, semantic ui is not working

--- src/components/Auth/Register/Register.component.jsx
+++ src/components/Auth/Register/Register.component.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import { Grid, Form, Segment } from 'semantic-ui-react';
 
 const Register = () => {
+
+  const handleInput = (e) => {
+
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <Grid verticalAlign="middle" textAlign="center">
+      <Grid.Column>
+        <Form>
+          <Segment stacked>
+            <Form.Input
+              name="userName"
+              value=""
+              icon="user"
+              iconPosition="left"
+              onChange={handleInput}
+              type="text"
+              placeholder="User Name"
+            />
+            <Form.Input
+              name="email"
+              value=""
+              icon="mail"
+              iconPosition="left"
+              onChange={handleInput}
+              type="email"
+              placeholder="User Email"
+            />
+            <Form.Input
+              name="password"
+              value=""
+              icon="lock"
+              iconPosition="left"
+              onChange={handleInput}
+              type="password"
+              placeholder="User Password"
+            />
+            <Form.Input
+              name="confirmPassword"
+              value=""
+              icon="user"
+              iconPosition="left"
+              onChange={handleInput}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
   )
 }
 
