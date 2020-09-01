8fdac5fb7e4896802890c7f574deb12c65b6ef38
Sone <sthaya@gmail.com>
2020-08-31 18:57:47 -0700

    useEffect connect to push after login

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 import Register from './components/Auth/Register/Register.component';
-import Login from './components/Auth/Login/Login.component'
+import Login from './components/Auth/Login/Login.component';
+import firebase from './server/firebase'
 
 import 'semantic-ui-css/semantic.min.css'
 
+const Index = (props) => {
 
-ReactDOM.render(
-  <>
-    <Router>
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        props.history.push("/");
+      } else {
+        props.history.push('/login')
+      }
+    })
+  }, []);
+
+  return (
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route exact path="/" component={App} />
       </Switch>
+  )
+}
+
+const IndexWithRouter = withRouter(Index);
+
+ReactDOM.render(
+  <>
+    <Router>
+      <IndexWithRouter />
     </Router>
   </>,
   document.getElementById('root')
