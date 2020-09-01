fd8029ece197c82638e79a691e8591a036899347
Sone <sthaya@gmail.com>
2020-08-31 19:16:08 -0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 import Register from './components/Auth/Register/Register.component';
 import Login from './components/Auth/Login/Login.component';
-import firebase from './server/firebase'
+import firebase from './server/firebase';
+import { combinedReducers } from './store/reducer';
+import { setUser } from './store/actioncreator';
 
 import 'semantic-ui-css/semantic.min.css'
 
@@ -19,8 +21,10 @@ const Index = (props) => {
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        props.setUser(user);
         props.history.push("/");
       } else {
+        props.setUser(null);
         props.history.push('/login')
       }
     })
@@ -35,7 +39,19 @@ const Index = (props) => {
   )
 }
 
-const IndexWithRouter = withRouter(Index);
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUser: (user) => { dispatch(setUser(user)) }
+  }
+}
+
+const IndexWithRouter = withRouter(connect(mapStateToProps, mapDispatchToProps)(Index));
 
 ReactDOM.render(
   <>
