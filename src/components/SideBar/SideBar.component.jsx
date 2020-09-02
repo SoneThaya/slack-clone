6ca4d87f43d783c0f3ae5481129b3c0c745d0a10
Sone <sthaya@gmail.com>
2020-09-01 20:18:04 -0700

    created userinfo for side bar component

--- src/components/SideBar/SideBar.component.jsx
+++ src/components/SideBar/SideBar.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react';
+import UserInfo from './UserInfo/UserInfo.component'
 
 import './SideBar.css'
 
 export const SideBar = () => {
   return (
     <Menu vertical fixed="left" borderless size="large" className="side_bar">
-      
+      <UserInfo />
     </Menu>
   )
 }
 
-export default SideBar
+export default SideBar;
