8153345c0d9c6cd2c72e8f83fdefd7e20444caf7
Sone <sthaya@gmail.com>
2020-09-03 18:24:12 -0700

    added sign out dropdown and styled

--- src/components/SideBar/UserInfo/UserInfo.component.jsx
+++ src/components/SideBar/UserInfo/UserInfo.component.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import { Grid, Header, Icon, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon, Image, Dropdown } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import firebase from '../../../server/firebase';
 
 import './UserInfo.css'
 
 const UserInfo = (props) => {
+
+  const getDropDownOptions = () => {
+    return [{
+      key: 'signout',
+      text: <span onClick={signOut}>
+        Sign Out
+      </span>
+    }]
+  }
+
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("user signed out"))
+  }
+
   if (props.user) {
 
     return (
       <Grid>
         <Grid.Column>
-          <Grid.Row>
-            <Header>
+          <Grid.Row className="userinfo_grid_row">
+            <Header inverted as="h2">
               <Icon name="slack" />
               <Header.Content>Slack</Header.Content>
             </Header>
-            <Header>
-              <span>
-                <Image src={props.user.photoUrl} avatar></Image>
-                {props.user.displayName}
-              </span>
+            <Header className="userinfo_displayName" inverted as="h4">
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={props.user.photoUrl} avatar></Image>
+                    {props.user.displayName}
+                  </span>
+              }
+                options={getDropDownOptions()}
+              >
+              
+              </Dropdown>
+              
             </Header>
           </Grid.Row>
         </Grid.Column>
