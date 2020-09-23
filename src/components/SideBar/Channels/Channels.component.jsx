906c6db94aacf553517fdabaa66f9f5781145674
Sone <sthaya@gmail.com>
2020-09-22 20:13:14 -0700

    set up on submit function to create channel

--- src/components/SideBar/Channels/Channels.component.jsx
+++ src/components/SideBar/Channels/Channels.component.jsx
@@ -1,60 +1,94 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux';
-import { Menu, Icon, Modal, Form, Button, Segment } from 'semantic-ui-react'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Menu, Icon, Modal, Form, Button, Segment } from "semantic-ui-react";
+import firebase from "../../../server/firebase";
 
-import './Channels.css'
+import "./Channels.css";
 
 const Channels = (props) => {
-
   const [modalOpenState, setModalOpenState] = useState(false);
-  const [channelAddState, setChannelAddState] = useState({});
+  const [channelAddState, setChannelAddState] = useState({
+    name: "",
+    description: "",
+  });
+  const channelsRef = firebase.database().ref("channels");
 
   const openModal = () => {
     setModalOpenState(true);
-  }
+  };
 
   const closeModal = () => {
     setModalOpenState(false);
-  }
+  };
 
   const onSubmit = () => {
 
+    if (!checkIfFormValid()) {
+      return
+    }
+
+    const key = channelsRef.push().key;
+
+    const channel = {
+      id: key,
+      name: channelAddState.name,
+      description: channelAddState.description,
+      created_by: {
+        name: props.user.displayName,
+        avatar: props.user.photoUrl,
+      },
+    };
+
+    channelsRef
+      .child(key)
+      .update(channel)
+      .then(() => {
+        setChannelAddState({ name: '', description: '' })
+        console.log('saved')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  };
+
+  const checkIfFormValid = () => {
+    return channelAddState && channelAddState.name && channelAddState.description
   }
 
   const handleInput = (e) => {
-
-  }
-
+    let target = e.target;
+    setChannelAddState((currentState) => {
+      let updatedState = { ...currentState };
+      updatedState[target.name] = target.value;
+      return updatedState;
+    });
+  };
 
   return (
     <>
-    <Menu.Menu>
-      <Menu.Item>
-        <span>
-          <Icon name="exchange" />Channels
-        </span>
-        (0)
-      </Menu.Item>
-      <Menu.Item>
-        <span onClick={openModal}>
-          <Icon name="add" /> ADD
-        </span>
-      </Menu.Item>
-    </Menu.Menu>
+      <Menu.Menu>
+        <Menu.Item>
+          <span>
+            <Icon name="exchange" />
+            Channels
+          </span>
+          (0)
+        </Menu.Item>
+        <Menu.Item>
+          <span onClick={openModal}>
+            <Icon name="add" /> ADD
+          </span>
+        </Menu.Item>
+      </Menu.Menu>
 
       <Modal open={modalOpenState} onClose={closeModal}>
-        <Modal.Header>
-         Create Channel
-        </Modal.Header>
+        <Modal.Header>Create Channel</Modal.Header>
         <Modal.Content>
           <Form onSubmit={onSubmit}>
             <Segment stacked>
-            
               <Form.Input
                 name="name"
-                value={channelAddState.Name}
-                
-                
+                value={channelAddState.name}
                 onChange={handleInput}
                 type="text"
                 placeholder="Enter Channel Name"
@@ -66,21 +100,26 @@ const Channels = (props) => {
                 type="text"
                 placeholder="Enter Channel Description"
               />
-              
             </Segment>
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button>
+          <Button onClick={onSubmit}>
             <Icon name="checkmark" /> Save
           </Button>
           <Button onClick={closeModal}>
             <Icon name="remove" /> Cancel
           </Button>
         </Modal.Actions>
-    </Modal>
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default connect()(Channels);
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+
+export default connect(mapStateToProps)(Channels);
